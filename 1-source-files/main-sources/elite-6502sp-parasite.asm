e10b80b4d0a304225674f3b1c14696f2c7436bf1
Mark Moxon <mark@moxon.net>
2022-10-05 09:52:36 +0100

    Minor save-related comment fixes

--- 1-source-files/main-sources/elite-6502sp-parasite.asm
+++ 1-source-files/main-sources/elite-6502sp-parasite.asm
@@ -2170,8 +2170,8 @@ LOAD_A% = LOAD%
 \ ------------------------------------------------------------------------------
 \
 \ The drive part of this string (the "0") is updated with the chosen drive in
-\ the QUS1 routine, but the directory part (the "E") is fixed. The variable is
-\ followed directly by the commander file at NA%, which starts with the
+\ the GTNMEW routine, but the directory part (the "E") is fixed. The variable
+\ is followed directly by the commander file at NA%, which starts with the
 \ commander name, so the full string at S1% is in the format ":0.E.JAMESON",
 \ which gives the full filename of the commander file.
 \
