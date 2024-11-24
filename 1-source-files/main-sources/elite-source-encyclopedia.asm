06ce26e32d205d9636d9feaf5052b89ec1c2d4ff
Mark Moxon <mark@moxon.net>
2024-11-24 13:59:42 +0000

    Improve layout of two-letter text token macros

--- 1-source-files/main-sources/elite-source-encyclopedia.asm
+++ 1-source-files/main-sources/elite-source-encyclopedia.asm
@@ -21131,48 +21131,169 @@ ENDMACRO
 
 MACRO ETWO t, k
 
- IF t = '-' AND k = '-' : EQUB 215 EOR VE : ENDIF
- IF t = 'A' AND k = 'B' : EQUB 216 EOR VE : ENDIF
- IF t = 'O' AND k = 'U' : EQUB 217 EOR VE : ENDIF
- IF t = 'S' AND k = 'E' : EQUB 218 EOR VE : ENDIF
- IF t = 'I' AND k = 'T' : EQUB 219 EOR VE : ENDIF
- IF t = 'I' AND k = 'L' : EQUB 220 EOR VE : ENDIF
- IF t = 'E' AND k = 'T' : EQUB 221 EOR VE : ENDIF
- IF t = 'S' AND k = 'T' : EQUB 222 EOR VE : ENDIF
- IF t = 'O' AND k = 'N' : EQUB 223 EOR VE : ENDIF
- IF t = 'L' AND k = 'O' : EQUB 224 EOR VE : ENDIF
- IF t = 'N' AND k = 'U' : EQUB 225 EOR VE : ENDIF
- IF t = 'T' AND k = 'H' : EQUB 226 EOR VE : ENDIF
- IF t = 'N' AND k = 'O' : EQUB 227 EOR VE : ENDIF
+ IF t = '-' AND k = '-'
+  EQUB 215 EOR VE
+ ENDIF
 
- IF t = 'A' AND k = 'L' : EQUB 228 EOR VE : ENDIF
- IF t = 'L' AND k = 'E' : EQUB 229 EOR VE : ENDIF
- IF t = 'X' AND k = 'E' : EQUB 230 EOR VE : ENDIF
- IF t = 'G' AND k = 'E' : EQUB 231 EOR VE : ENDIF
- IF t = 'Z' AND k = 'A' : EQUB 232 EOR VE : ENDIF
- IF t = 'C' AND k = 'E' : EQUB 233 EOR VE : ENDIF
- IF t = 'B' AND k = 'I' : EQUB 234 EOR VE : ENDIF
- IF t = 'S' AND k = 'O' : EQUB 235 EOR VE : ENDIF
- IF t = 'U' AND k = 'S' : EQUB 236 EOR VE : ENDIF
- IF t = 'E' AND k = 'S' : EQUB 237 EOR VE : ENDIF
- IF t = 'A' AND k = 'R' : EQUB 238 EOR VE : ENDIF
- IF t = 'M' AND k = 'A' : EQUB 239 EOR VE : ENDIF
- IF t = 'I' AND k = 'N' : EQUB 240 EOR VE : ENDIF
- IF t = 'D' AND k = 'I' : EQUB 241 EOR VE : ENDIF
- IF t = 'R' AND k = 'E' : EQUB 242 EOR VE : ENDIF
- IF t = 'A' AND k = '?' : EQUB 243 EOR VE : ENDIF
- IF t = 'E' AND k = 'R' : EQUB 244 EOR VE : ENDIF
- IF t = 'A' AND k = 'T' : EQUB 245 EOR VE : ENDIF
- IF t = 'E' AND k = 'N' : EQUB 246 EOR VE : ENDIF
- IF t = 'B' AND k = 'E' : EQUB 247 EOR VE : ENDIF
- IF t = 'R' AND k = 'A' : EQUB 248 EOR VE : ENDIF
- IF t = 'L' AND k = 'A' : EQUB 249 EOR VE : ENDIF
- IF t = 'V' AND k = 'E' : EQUB 250 EOR VE : ENDIF
- IF t = 'T' AND k = 'I' : EQUB 251 EOR VE : ENDIF
- IF t = 'E' AND k = 'D' : EQUB 252 EOR VE : ENDIF
- IF t = 'O' AND k = 'R' : EQUB 253 EOR VE : ENDIF
- IF t = 'Q' AND k = 'U' : EQUB 254 EOR VE : ENDIF
- IF t = 'A' AND k = 'N' : EQUB 255 EOR VE : ENDIF
+ IF t = 'A' AND k = 'B'
+  EQUB 216 EOR VE
+ ENDIF
+
+ IF t = 'O' AND k = 'U'
+  EQUB 217 EOR VE
+ ENDIF
+
+ IF t = 'S' AND k = 'E'
+  EQUB 218 EOR VE
+ ENDIF
+
+ IF t = 'I' AND k = 'T'
+  EQUB 219 EOR VE
+ ENDIF
+
+ IF t = 'I' AND k = 'L'
+  EQUB 220 EOR VE
+ ENDIF
+
+ IF t = 'E' AND k = 'T'
+  EQUB 221 EOR VE
+ ENDIF
+
+ IF t = 'S' AND k = 'T'
+  EQUB 222 EOR VE
+ ENDIF
+
+ IF t = 'O' AND k = 'N'
+  EQUB 223 EOR VE
+ ENDIF
+
+ IF t = 'L' AND k = 'O'
+  EQUB 224 EOR VE
+ ENDIF
+
+ IF t = 'N' AND k = 'U'
+  EQUB 225 EOR VE
+ ENDIF
+
+ IF t = 'T' AND k = 'H'
+  EQUB 226 EOR VE
+ ENDIF
+
+ IF t = 'N' AND k = 'O'
+  EQUB 227 EOR VE
+ ENDIF
+
+ IF t = 'A' AND k = 'L'
+  EQUB 228 EOR VE
+ ENDIF
+
+ IF t = 'L' AND k = 'E'
+  EQUB 229 EOR VE
+ ENDIF
+
+ IF t = 'X' AND k = 'E'
+  EQUB 230 EOR VE
+ ENDIF
+
+ IF t = 'G' AND k = 'E'
+  EQUB 231 EOR VE
+ ENDIF
+
+ IF t = 'Z' AND k = 'A'
+  EQUB 232 EOR VE
+ ENDIF
+
+ IF t = 'C' AND k = 'E'
+  EQUB 233 EOR VE
+ ENDIF
+
+ IF t = 'B' AND k = 'I'
+  EQUB 234 EOR VE
+ ENDIF
+
+ IF t = 'S' AND k = 'O'
+  EQUB 235 EOR VE
+ ENDIF
+
+ IF t = 'U' AND k = 'S'
+  EQUB 236 EOR VE
+ ENDIF
+
+ IF t = 'E' AND k = 'S'
+  EQUB 237 EOR VE
+ ENDIF
+
+ IF t = 'A' AND k = 'R'
+  EQUB 238 EOR VE
+ ENDIF
+
+ IF t = 'M' AND k = 'A'
+  EQUB 239 EOR VE
+ ENDIF
+
+ IF t = 'I' AND k = 'N'
+  EQUB 240 EOR VE
+ ENDIF
+
+ IF t = 'D' AND k = 'I'
+  EQUB 241 EOR VE
+ ENDIF
+
+ IF t = 'R' AND k = 'E'
+  EQUB 242 EOR VE
+ ENDIF
+
+ IF t = 'A' AND k = '?'
+  EQUB 243 EOR VE
+ ENDIF
+
+ IF t = 'E' AND k = 'R'
+  EQUB 244 EOR VE
+ ENDIF
+
+ IF t = 'A' AND k = 'T'
+  EQUB 245 EOR VE
+ ENDIF
+
+ IF t = 'E' AND k = 'N'
+  EQUB 246 EOR VE
+ ENDIF
+
+ IF t = 'B' AND k = 'E'
+  EQUB 247 EOR VE
+ ENDIF
+
+ IF t = 'R' AND k = 'A'
+  EQUB 248 EOR VE
+ ENDIF
+
+ IF t = 'L' AND k = 'A'
+  EQUB 249 EOR VE
+ ENDIF
+
+ IF t = 'V' AND k = 'E'
+  EQUB 250 EOR VE
+ ENDIF
+
+ IF t = 'T' AND k = 'I'
+  EQUB 251 EOR VE
+ ENDIF
+
+ IF t = 'E' AND k = 'D'
+  EQUB 252 EOR VE
+ ENDIF
+
+ IF t = 'O' AND k = 'R'
+  EQUB 253 EOR VE
+ ENDIF
+
+ IF t = 'Q' AND k = 'U'
+  EQUB 254 EOR VE
+ ENDIF
+
+ IF t = 'A' AND k = 'N'
+  EQUB 255 EOR VE
+ ENDIF
 
 ENDMACRO
 
--- 1-source-files/main-sources/elite-text-tokens.asm
+++ 1-source-files/main-sources/elite-text-tokens.asm
@@ -129,38 +129,133 @@ ENDMACRO
 
 MACRO TWOK t, k
 
- IF t = 'A' AND k = 'L' : EQUB 128 EOR RE : ENDIF
- IF t = 'L' AND k = 'E' : EQUB 129 EOR RE : ENDIF
- IF t = 'X' AND k = 'E' : EQUB 130 EOR RE : ENDIF
- IF t = 'G' AND k = 'E' : EQUB 131 EOR RE : ENDIF
- IF t = 'Z' AND k = 'A' : EQUB 132 EOR RE : ENDIF
- IF t = 'C' AND k = 'E' : EQUB 133 EOR RE : ENDIF
- IF t = 'B' AND k = 'I' : EQUB 134 EOR RE : ENDIF
- IF t = 'S' AND k = 'O' : EQUB 135 EOR RE : ENDIF
- IF t = 'U' AND k = 'S' : EQUB 136 EOR RE : ENDIF
- IF t = 'E' AND k = 'S' : EQUB 137 EOR RE : ENDIF
- IF t = 'A' AND k = 'R' : EQUB 138 EOR RE : ENDIF
- IF t = 'M' AND k = 'A' : EQUB 139 EOR RE : ENDIF
- IF t = 'I' AND k = 'N' : EQUB 140 EOR RE : ENDIF
- IF t = 'D' AND k = 'I' : EQUB 141 EOR RE : ENDIF
- IF t = 'R' AND k = 'E' : EQUB 142 EOR RE : ENDIF
- IF t = 'A' AND k = '?' : EQUB 143 EOR RE : ENDIF
- IF t = 'E' AND k = 'R' : EQUB 144 EOR RE : ENDIF
- IF t = 'A' AND k = 'T' : EQUB 145 EOR RE : ENDIF
- IF t = 'E' AND k = 'N' : EQUB 146 EOR RE : ENDIF
- IF t = 'B' AND k = 'E' : EQUB 147 EOR RE : ENDIF
- IF t = 'R' AND k = 'A' : EQUB 148 EOR RE : ENDIF
- IF t = 'L' AND k = 'A' : EQUB 149 EOR RE : ENDIF
- IF t = 'V' AND k = 'E' : EQUB 150 EOR RE : ENDIF
- IF t = 'T' AND k = 'I' : EQUB 151 EOR RE : ENDIF
- IF t = 'E' AND k = 'D' : EQUB 152 EOR RE : ENDIF
- IF t = 'O' AND k = 'R' : EQUB 153 EOR RE : ENDIF
- IF t = 'Q' AND k = 'U' : EQUB 154 EOR RE : ENDIF
- IF t = 'A' AND k = 'N' : EQUB 155 EOR RE : ENDIF
- IF t = 'T' AND k = 'E' : EQUB 156 EOR RE : ENDIF
- IF t = 'I' AND k = 'S' : EQUB 157 EOR RE : ENDIF
- IF t = 'R' AND k = 'I' : EQUB 158 EOR RE : ENDIF
- IF t = 'O' AND k = 'N' : EQUB 159 EOR RE : ENDIF
+ IF t = 'A' AND k = 'L'
+  EQUB 128 EOR RE
+ ENDIF
+
+ IF t = 'L' AND k = 'E'
+  EQUB 129 EOR RE
+ ENDIF
+
+ IF t = 'X' AND k = 'E'
+  EQUB 130 EOR RE
+ ENDIF
+
+ IF t = 'G' AND k = 'E'
+  EQUB 131 EOR RE
+ ENDIF
+
+ IF t = 'Z' AND k = 'A'
+  EQUB 132 EOR RE
+ ENDIF
+
+ IF t = 'C' AND k = 'E'
+  EQUB 133 EOR RE
+ ENDIF
+
+ IF t = 'B' AND k = 'I'
+  EQUB 134 EOR RE
+ ENDIF
+
+ IF t = 'S' AND k = 'O'
+  EQUB 135 EOR RE
+ ENDIF
+
+ IF t = 'U' AND k = 'S'
+  EQUB 136 EOR RE
+ ENDIF
+
+ IF t = 'E' AND k = 'S'
+  EQUB 137 EOR RE
+ ENDIF
+
+ IF t = 'A' AND k = 'R'
+  EQUB 138 EOR RE
+ ENDIF
+
+ IF t = 'M' AND k = 'A'
+  EQUB 139 EOR RE
+ ENDIF
+
+ IF t = 'I' AND k = 'N'
+  EQUB 140 EOR RE
+ ENDIF
+
+ IF t = 'D' AND k = 'I'
+  EQUB 141 EOR RE
+ ENDIF
+
+ IF t = 'R' AND k = 'E'
+  EQUB 142 EOR RE
+ ENDIF
+
+ IF t = 'A' AND k = '?'
+  EQUB 143 EOR RE
+ ENDIF
+
+ IF t = 'E' AND k = 'R'
+  EQUB 144 EOR RE
+ ENDIF
+
+ IF t = 'A' AND k = 'T'
+  EQUB 145 EOR RE
+ ENDIF
+
+ IF t = 'E' AND k = 'N'
+  EQUB 146 EOR RE
+ ENDIF
+
+ IF t = 'B' AND k = 'E'
+  EQUB 147 EOR RE
+ ENDIF
+
+ IF t = 'R' AND k = 'A'
+  EQUB 148 EOR RE
+ ENDIF
+
+ IF t = 'L' AND k = 'A'
+  EQUB 149 EOR RE
+ ENDIF
+
+ IF t = 'V' AND k = 'E'
+  EQUB 150 EOR RE
+ ENDIF
+
+ IF t = 'T' AND k = 'I'
+  EQUB 151 EOR RE
+ ENDIF
+
+ IF t = 'E' AND k = 'D'
+  EQUB 152 EOR RE
+ ENDIF
+
+ IF t = 'O' AND k = 'R'
+  EQUB 153 EOR RE
+ ENDIF
+
+ IF t = 'Q' AND k = 'U'
+  EQUB 154 EOR RE
+ ENDIF
+
+ IF t = 'A' AND k = 'N'
+  EQUB 155 EOR RE
+ ENDIF
+
+ IF t = 'T' AND k = 'E'
+  EQUB 156 EOR RE
+ ENDIF
+
+ IF t = 'I' AND k = 'S'
+  EQUB 157 EOR RE
+ ENDIF
+
+ IF t = 'R' AND k = 'I'
+  EQUB 158 EOR RE
+ ENDIF
+
+ IF t = 'O' AND k = 'N'
+  EQUB 159 EOR RE
+ ENDIF
 
 ENDMACRO
 
