8e980d0a72edc7a9a2fa335968cf589f9f01af1e
Mark Moxon <mark@moxon.net>
2024-11-28 13:32:17 +0000

    Fix an incorrect reference to line-drawing in the pixel routine

--- 1-source-files/main-sources/elite-source-encyclopedia.asm
+++ 1-source-files/main-sources/elite-source-encyclopedia.asm
@@ -5497,11 +5497,11 @@ ENDIF
  STA SC
 
  TYA                    \ Set Y = Y mod 8, which is the pixel row within the
- AND #7                 \ character block at which we want to draw the start of
- TAY                    \ our line (as each character block has 8 rows)
+ AND #7                 \ character block at which we want to draw our pixel
+ TAY                    \ (as each character block has 8 rows)
 
  TXA                    \ Set X = X mod 8, which is the horizontal pixel number
- AND #7                 \ within the character block where the line starts (as
+ AND #7                 \ within the character block where the pixel lies (as
  TAX                    \ each pixel line in the character block is 8 pixels
                         \ wide)
 
